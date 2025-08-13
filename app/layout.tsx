15aa35a5805d168b4878b42a06a2f0361702b671
aashirisrar123-beep <aashirisrar123@gmail.com>
2025-08-13 13:20:04 +0500

    Auto update app/layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import "./globals.css";
+import { Inter } from 'next/font/google';
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+});
+
+export default function RootLayout({ children }: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} font-sans`}>
+        {children}
+      </body>
+    </html>
+  );
+}
